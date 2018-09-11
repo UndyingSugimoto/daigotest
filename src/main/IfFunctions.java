6bd818fb57b0a5f6fe6eae86f246a1272b2ab8eb
kouji sugimoto <kouji36000@gmail.com>
2018-09-12 00:17:54 +0900

    9/11 if文の問題追加

---
+++ src/main/IfFunctions.java
@@ -0,0 +1,37 @@
+package main;
+
+public class IfFunctions {
+
+	//if文、条件分岐文のテスト
+
+	//引数が偶数なら true を、それ以外ならfalse を返す
+	public boolean isEvenNumber(int tag) {
+		//TODO 実装しろ
+		return false;
+	}
+
+	//引数が1なら '一' を、2なら '二' を、3なら '三' を、それ以外なら '他' を返す
+	public String pattern(int tag) {
+		//TODO 実装しろ
+		return "";
+	}
+
+	//① leftとrightが同じなら 0 を返す
+	//② leftがrightより小さいなら 1 を返す
+	//③ leftがrightより大きいなら -1 を返す
+
+	public int compareValues(int left, int right) {
+		//TODO 実装しろ
+		return 0;
+	}
+
+	//①  配列の全ての合計値が偶数なら 1 を返す
+	//②  配列の全ての合計値が奇数なら -1を返す
+	//③  配列の全ての合計値が0なら 0 を返す
+
+	public int sumAllVauesCompareEvenNumber(int[] tag) {
+		//TODO 実装しろ
+		return 0;
+	}
+
+}
---
+++ src/test/IfFunctionsTest.java
@@ -0,0 +1,210 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.jupiter.api.Test;
+
+import main.IfFunctions;
+
+class IfFunctionsTest {
+
+	@Test
+	public void isEvenNumber1() {
+		IfFunctions target = new IfFunctions();
+
+		int tag = 2;
+
+		boolean result = target.isEvenNumber(tag);
+		boolean expext = true;//期待される値
+
+		// この二つは一緒ですか？
+		assertEquals(expext, result);//期待される値、実際の値
+
+	}
+
+	@Test
+	public void isEvenNumber2() {
+		IfFunctions target = new IfFunctions();
+
+		int tag = 1;
+
+		boolean result = target.isEvenNumber(tag);
+		boolean expext = false;//期待される値
+
+		// この二つは一緒ですか？
+		assertEquals(expext, result);//期待される値、実際の値
+
+	}
+
+	@Test
+	public void isEvenNumber3() {
+		IfFunctions target = new IfFunctions();
+
+		int tag = 0;
+
+		boolean result = target.isEvenNumber(tag);
+		boolean expext = false;//期待される値
+
+		// この二つは一緒ですか？
+		assertEquals(expext, result);//期待される値、実際の値
+
+	}
+
+	@Test
+	public void pattern1() {
+		IfFunctions target = new IfFunctions();
+
+		int tag = 1;
+
+		String result = target.pattern(tag);
+		String expext = "一";//期待される値
+
+		// この二つは一緒ですか？
+		assertEquals(expext, result);//期待される値、実際の値
+
+	}
+
+	@Test
+	public void pattern2() {
+		IfFunctions target = new IfFunctions();
+
+		int tag = 2;
+
+		String result = target.pattern(tag);
+		String expext = "二";//期待される値
+
+		// この二つは一緒ですか？
+		assertEquals(expext, result);//期待される値、実際の値
+
+	}
+
+	@Test
+	public void pattern3() {
+		IfFunctions target = new IfFunctions();
+
+		int tag = 3;
+
+		String result = target.pattern(tag);
+		String expext = "三";//期待される値
+
+		// この二つは一緒ですか？
+		assertEquals(expext, result);//期待される値、実際の値
+
+	}
+
+	@Test
+	public void pattern4() {
+		IfFunctions target = new IfFunctions();
+
+		int tag = 0;
+
+		String result = target.pattern(tag);
+		String expext = "他";//期待される値
+
+		// この二つは一緒ですか？
+		assertEquals(expext, result);//期待される値、実際の値
+
+	}
+
+	@Test
+	public void pattern5() {
+		IfFunctions target = new IfFunctions();
+
+		int tag = 4;
+
+		String result = target.pattern(tag);
+		String expext = "他";//期待される値
+
+		// この二つは一緒ですか？
+		assertEquals(expext, result);//期待される値、実際の値
+
+	}
+
+	@Test
+	public void compareValues1() {
+		IfFunctions target = new IfFunctions();
+
+		int tag1 = 1;
+		int tag2 = 1;
+
+		int result = target.compareValues(tag1, tag2);
+		int expext = 0;//期待される値
+
+		// この二つは一緒ですか？
+		assertEquals(expext, result);//期待される値、実際の値
+
+	}
+
+	@Test
+	public void compareValues2() {
+		IfFunctions target = new IfFunctions();
+
+		int tag1 = 1;
+		int tag2 = 2;
+
+		int result = target.compareValues(tag1, tag2);
+		int expext = 1;//期待される値
+
+		// この二つは一緒ですか？
+		assertEquals(expext, result);//期待される値、実際の値
+
+	}
+
+	@Test
+	public void compareValues3() {
+		IfFunctions target = new IfFunctions();
+
+		int tag1 = 2;
+		int tag2 = 1;
+
+		int result = target.compareValues(tag1, tag2);
+		int expext = -1;//期待される値
+
+		// この二つは一緒ですか？
+		assertEquals(expext, result);//期待される値、実際の値
+
+	}
+
+	@Test
+	public void sumAllVauesCompareEvenNumbe1r() {
+		IfFunctions target = new IfFunctions();
+
+		int[] tag = { 1, 2, 3, 4 };
+
+		int result = target.sumAllVauesCompareEvenNumber(tag);
+		int expext = 1;//期待される値
+
+		// この二つは一緒ですか？
+		assertEquals(expext, result);//期待される値、実際の値
+
+	}
+
+	@Test
+	public void sumAllVauesCompareEvenNumber2() {
+		IfFunctions target = new IfFunctions();
+
+		int[] tag = { 1, 2, 3, 4, 5 };
+
+		int result = target.sumAllVauesCompareEvenNumber(tag);
+		int expext = -1;//期待される値
+
+		// この二つは一緒ですか？
+		assertEquals(expext, result);//期待される値、実際の値
+
+	}
+
+	@Test
+	public void sumAllVauesCompareEvenNumber3() {
+		IfFunctions target = new IfFunctions();
+
+		int[] tag = { 0, 0, 0, 0, 0 };
+
+		int result = target.sumAllVauesCompareEvenNumber(tag);
+		int expext = 0;//期待される値
+
+		// この二つは一緒ですか？
+		assertEquals(expext, result);//期待される値、実際の値
+
+	}
+
+}
